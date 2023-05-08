fbb69666c4fd2e42456e41e67b4d6f163d0578f2
yerman <110374480+ger-rinaldi@users.noreply.github.com>
2023-05-07 22:33:44 -0300

    Get data required for visualization

---
+++ script.js
@@ -0,0 +1,11 @@
+const data_src = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+const data_request = new XMLHttpRequest();
+
+data_request.open("GET", data_src, true);
+
+data_request.onload = function(){
+  const data = JSON.parse(data_request.responseText).data;
+  console.log(data);
+};
+
+data_request.send();
