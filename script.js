63e9421ad73d606ccf458a5d252d4101b9b9ba17
yerman <110374480+ger-rinaldi@users.noreply.github.com>
2023-05-08 04:28:27 -0300

    Format request as main func to improve readability

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-const data_src = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
-const data_request = new XMLHttpRequest();
+function main(){
 
-data_request.open("GET", data_src, true);
+  const data_src = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+  const data_request = new XMLHttpRequest();
 
-data_request.onload = function(){
-  const data = JSON.parse(data_request.responseText).data;
-  console.log(data);
+  data_request.open("GET", data_src, true);
+  
+  data_request.onload = function(){
+    const data = JSON.parse(data_request.responseText).data;
+    console.log(data);
+    render_bars(data);
+  };
+
+  data_request.send();
 };
-
-data_request.send();
